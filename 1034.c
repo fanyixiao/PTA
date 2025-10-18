337367f513c5bbde6b487b85fa42009a9502a598
Fanyixiao <fanyixiao2006@outlook.com>
2025-10-18 18:58:22 +0800

    1034

--- 1034.c
+++ 1034.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 #include<math.h>
 struct intput {
-	int a1, b1, a2, b2;
+	long int a1, b1, a2, b2;
 }num;
-int gcb(int num1,int num2);
-int output(int numerator,int denominator);
+long int gcb(long int num1,long int num2);
+int output(long int numerator,long int denominator);
 int main(void) {
-	int i,integer1,integer2;
-	scanf("%d/%d %d/%d", &num.a1, &num.b1, &num.a2, &num.b2);
+	long int i,integer1,integer2;
+	scanf("%ld/%ld %ld/%ld", &num.a1, &num.b1, &num.a2, &num.b2);
 	for (i = 0; i <= 3; i++) {
 		output(num.a1, num.b1);
 		switch (i) {
@@ -49,8 +49,9 @@ int main(void) {
 	return 0;
 }
 
-int gcb(int num1, int num2) {
-	int num;
+long int gcd(long int num1, long int num2) {
+	long int num;
+	if (num2 == 0) return num1;
 	while (num1 % num2 != 0) {
 		num = num1 % num2;
 		num1 = num2;
@@ -60,38 +61,42 @@ int gcb(int num1, int num2) {
 		num = num2;
 	return num;
 }
-int output(int numerator, int denominator) {
-	int integer,num;
-	if (denominator < 0) {
-		numerator *= -1;
-		denominator *= -1;
-	}
-	else if (denominator == 0) {
-		printf("Inf");
-		return 0;
-	}
-	integer = numerator / denominator;
+int output(long int numerator, long int denominator) {
+	long int integer,num;
 	if (numerator == 0) {
 		printf("0");
 		return 0;
 	}
-	if(integer<0)
-		numerator = abs(numerator);
-	numerator -= abs(integer) * denominator;
-	num = abs(gcb(numerator, denominator));
+	if (denominator < 0) {
+		numerator *= -1;
+		denominator *= -1;
+	}
+	if (denominator == 0) {
+		printf("Inf");
+		return 0;
+	}
+	integer = numerator / denominator;
+	int is_negative = (numerator < 0);
+	numerator %= denominator;
+	if (numerator < 0)
+		numerator = -numerator;
+	num = (long int)abs(gcd(numerator, denominator));
 	numerator /= num;
 	denominator /= num;
-	if (numerator < 0||integer<0)
+	if (is_negative)
 		printf("(");
 	if (integer != 0) {
 		if (numerator != 0)
-			printf("%d %d/%d", integer, numerator, denominator);
+			printf("%ld %ld/%ld", integer, numerator, denominator);
 		else if (numerator == 0)
-			printf("%d", integer);
+			printf("%ld", integer);
 	}
-	else if (integer == 0)
-		printf("%d/%d", numerator, denominator);
-	if (numerator < 0||integer<0)
+	else if (integer == 0) {
+		if (is_negative)
+			numerator = -numerator;
+		printf("%ld/%ld", numerator, denominator);
+	}
+	if (is_negative)
 		printf(")");
 	return 0;
 }
